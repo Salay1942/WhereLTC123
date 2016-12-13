6859f1fd35d5414da86807aba755d9512795dc73
macintosh <l.bounlerth@gmail.com>
2016-12-13 16:38:55 +0700

    No active bar finish

--- app/src/main/java/org/project/ltc/whereltc/Main2Activity.java
+++ app/src/main/java/org/project/ltc/whereltc/Main2Activity.java
@@ -2,12 +2,26 @@ package org.project.ltc.whereltc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
+
+    private EditText editText1;
+    private EditText editText2;
+    private EditText editText3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        editText1 = (EditText) findViewById(R.id.editText1);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        editText3 = (EditText) findViewById(R.id.editText3);
+    }
+
+    public void clickSignUpSign(View view) {
+
     }
 }
